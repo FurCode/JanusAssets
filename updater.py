0f7ee0f7866a890b944d579e9bbc987c9b120628
Foxlet <inquiries@comprepair.tk>
2015-04-29 09:09:40 -0400

    Update updater.py

--- updater.py
+++ updater.py
@@ -5,5 +5,5 @@ URL_UPDATE = "http://www.dgp.toronto.edu/~mccrae/projects/firebox/version.html"
 check = requests.get(URL_UPDATE)
 version_online = check.text
 
-if version_online < "41.8":
+if version_online > "41.8":
     print "New Version out."
